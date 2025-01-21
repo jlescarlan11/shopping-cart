0d79e8f26ba8dde55be469c4872dc7713089e9bd
john lester escarlan <jlescarlan11@gmail.com>
2025-01-21 20:43:48 +0800

    fixed the shop layout

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -24,23 +24,35 @@ const Shop = () => {
     },
   ];
   return (
-    <div className="bg-[var(--bg-color)] min-h-[calc(100vh-var(--header-height-sm))] md:min-h-[calc(100vh-var(--header-height-md))] lg:min-h-[calc(100vh-var(--header-height-lg))] text-[var(--text-color)] px-8 sm:px-24 lg:px-72 grid sample gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {items.map((item, index) => (
-        <div
-          key={index}
-          className="min-w-24  border  bg-slate-80 h-60 relative"
-        >
-          <div className="relative">
-            <div className="absolute bottom-0 left-0 right-0 text-center bg-slate-700 bg-opacity-20">
-              <span className="">{item.name}</span>
-              <br />
-              <span className="">{item.price}</span>
+    <>
+      <div className="bg-[var(--bg-color)] min-h-[calc(100vh-var(--header-height-sm))] md:min-h-[calc(100vh-var(--header-height-md))] lg:min-h-[calc(100vh-var(--header-height-lg))] text-[var(--text-color)] px-8 sm:px-24 lg:px-72 grid sample gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className="min-w-24 h-fit  border  bg-slate-80 relative"
+          >
+            <div className="">
+              <img
+                src={item.img}
+                className="w-auto h-52 border border-red-500"
+              />
+              <div className="flex justify-between bg-slate-700 bg-opacity-20 items-center p-4">
+                <div className="  ">
+                  <span className="">{item.name}</span>
+                  <br />
+                  <span className="">{item.price}</span>
+                </div>
+                <div>
+                  <span class="material-symbols-outlined">
+                    add_shopping_cart
+                  </span>
+                </div>
+              </div>
             </div>
-            <img src={item.img} className="w-auto h-60" />
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
