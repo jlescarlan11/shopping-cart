6f0181fb55e975087c9016e41be680f2d09a7b32
john lester escarlan <jlescarlan11@gmail.com>
2025-01-24 20:38:17 +0800

    designed the shop and activated the filtering in shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,6 +3,15 @@ import { use } from "react";
 
 const Shop = () => {
   const [items, setItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState(items);
+
+  const categories = [
+    { label: "All", category: "" },
+    { label: "Men", category: "men's clothing" },
+    { label: "Women", category: "women's clothing" },
+    { label: "Jewelery", category: "jewelery" },
+    { label: "Electronics", category: "electronics" },
+  ];
 
   // const items = [
   //   {
@@ -33,37 +42,65 @@ const Shop = () => {
     const fetchItems = async () => {
       const response = await fetch(`${BASE_URL}`);
       const items = await response.json();
+      console.log(items);
       setItems(items);
     };
 
     fetchItems();
   }, []);
 
+  useEffect(() => {
+    setFilteredItems(items);
+  }, [items]);
+
   return (
     <>
-      <div>
-        <ul>
-          <li>All</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Accesories</li>
+      <nav className="bg-[var(--bg-color)]">
+        <ul className="flex justify-center gap-4">
+          {categories.map((category, index) => (
+            <li
+              key={index}
+              className="p-4"
+              onClick={() => {
+                const filteredItems = items.filter((item) =>
+                  category.category
+                    ? item.category === category.category
+                    : item.category === item.category
+                );
+                setFilteredItems(filteredItems);
+              }}
+            >
+              {category.label}
+            </li>
+          ))}
         </ul>
-      </div>
-      <div className="bg-[var(--bg-color)] min-h-[calc(100vh-var(--header-height-sm))]  md:min-h-[calc(100vh-var(--header-height-md))] lg:min-h-[calc(100vh-var(--header-height-lg))] text-[var(--text-color)] px-8 sm:px-24 lg:px-72 grid sample gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {items.map((item, index) => (
-          <div key={item.id} className="min-w-24 h-auto bg-[#ffffff]">
-            <div className="flex flex-col">
+      </nav>
+      <div className="bg-[var(--bg-color)] min-h-[calc(100vh-var(--header-height-sm))]  md:min-h-[calc(100vh-var(--header-height-md))] lg:min-h-[calc(100vh-var(--header-height-lg))] text-[var(--text-color)] py-8 px-8 sm:px-24 lg:px-72 grid sample gap-16 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {filteredItems.map((item, index) => (
+          <div key={item.id} className="min-w-24 bg-[#ffffff] flex flex-col">
+            <div className="flex flex-col h-96">
               <img
                 src={item.image}
                 className="w-auto h-52 bg-transparent p-4"
               />
-              <div className="flex justify-between items-center p-4">
-                <div className="text-xs">
-                  <span className="">{item.title}</span>
-                  <br />
-                  <span className="">{item.price}</span>
+              <div className="flex items-center justify-between p-4 h-[calc(100%-13rem)] gap-4">
+                <div className="text-xs h-full flex flex-col justify-between items-baseline">
+                  <div>
+                    <span className="">{item.title}</span>
+                  </div>
+                  <div className="">
+                    <span className="">{item.price}</span>
+                  </div>
                 </div>
-                <div>
+                <div className="text-xs h-full flex flex-col justify-between items-center">
+                  <div>
+                    <span className="flex items-center">
+                      {item.rating.rate}{" "}
+                      <span className="material-symbols-outlined text-sm">
+                        star
+                      </span>
+                    </span>
+                  </div>
                   <span className="material-symbols-outlined">
                     add_shopping_cart
                   </span>
